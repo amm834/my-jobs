f23c7113a544e87cb9a22bb6d1b04a723e515e9f
Aung Myat Moe <65492233+amm834@users.noreply.github.com>
2023-05-15 17:04:31 +0630

    feat: add job list

--- components/Welcome.jsx
+++ components/Welcome.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import {Alert, Button, Text, TextInput, TouchableOpacity, View} from 'react-native';
+import React, {useState} from 'react';
+import {FlatList, Text, TextInput, TouchableOpacity, View} from 'react-native';
 import {MagnifyingGlassIcon} from 'react-native-heroicons/solid'
-import {Link, useRouter} from "expo-router";
+import {useRouter} from "expo-router";
 import colors from "../styles/colors";
+
+const jobTypes = ['Full Time', 'Part Time', 'Contractor']
 
 const Welcome = () => {
     const router = useRouter()
+    const [selectedJobType, setSelectedJobType] = useState("Full Time")
     return (
-        <>
+        <View>
             <View>
                 <Text
                     style={{
@@ -21,7 +24,7 @@ const Welcome = () => {
                     }}>Find your perfect job</Text>
             </View>
 
-            {/* serach box */}
+            {/* search box */}
             <View style={{
                 flexDirection: "row",
                 justifyContent: "space-between",
@@ -51,7 +54,37 @@ const Welcome = () => {
                     <MagnifyingGlassIcon color="white"/>
                 </TouchableOpacity>
             </View>
-        </>
+
+            <View style={{
+                marginTop: 10,
+            }}>
+                <FlatList
+                    data={jobTypes}
+                    horizontal
+                    contentContainerStyle={{
+                        columnGap: 8,
+                    }}
+                    renderItem={({item}) => {
+                        return (
+                            <TouchableOpacity style={{
+                                borderRadius: "15%",
+                                borderColor: selectedJobType === item ? colors.indigo500 : colors.indigo300,
+                                borderWidth: 1,
+                                paddingVertical: 6,
+                                paddingHorizontal: 8,
+                            }}
+                                              onPress={() => setSelectedJobType(item)}
+                            >
+                                <Text style={{
+                                    color: item === selectedJobType ? colors.gray800 : colors.gray500,
+                                }}>{item}</Text>
+                            </TouchableOpacity>
+                        )
+                    }}
+                    keyExtractor={item => item}
+                />
+            </View>
+        </View>
     );
 };
 
--- styles/colors.js
+++ styles/colors.js
@@ -1,6 +1,11 @@
 export default {
     primary: '#f9fafb',
     gray: '#111827',
+    gray500:'#64748b',
+    gray800:'#334155',
     indigo:'#6366f1',
     indigo50:'#eef2ff',
+    indigo100:'#e0e7ff',
+    indigo300:'#c4b5fd',
+    indigo500:'#6366f1',
 }
