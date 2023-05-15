aa5af58695f13f8f00aa8b89ee9ab094a982881f
Aung Myat Moe <65492233+amm834@users.noreply.github.com>
2023-05-15 22:02:24 +0630

    feat: add palceholder image

--- components/cards/PopularCard.jsx
+++ components/cards/PopularCard.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Image, Text, TouchableOpacity, View} from "react-native";
 import colors from "../../styles/colors";
 import fonts from "../../styles/fonts";
+import {checkImageURL} from "../../utils/checkImage";
 
 const PopularCard = ({
                          item,
@@ -15,7 +16,7 @@ const PopularCard = ({
                 borderRadius: 10,
                 marginVertical: 5,
                 height: 'auto',
-                width:200,
+                width: 200,
             }}
         >
             <TouchableOpacity
@@ -24,19 +25,34 @@ const PopularCard = ({
                     height: 40,
                     backgroundColor: colors.gray100,
                     borderRadius: 10,
+                    shadowColor: colors.gray100,
+                    shadowOffset: {
+                        width: 0,
+                        height: 2,
+                    },
+                    shadowOpacity: 0.25,
+                    shadowRadius: 3.84,
+                    elevation: 5,
                 }}
                 onPress={() => onCardPress(selectedJobType, item)}
             >
                 <Image
-                    source={{uri: item.employer_logo}}
+                    source={{
+                        uri: checkImageURL(item.employer_logo)
+                            ? item.employer_logo
+                            : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg"
+                    }}
                     resizeMode={'contain'}
-                    style={{width: 40, height: 40}}
+                    style={{
+                        width: '100%',
+                        height: '100%',
+                    }}
                 />
             </TouchableOpacity>
             <Text style={{
                 color: colors.gray500,
                 fontSize: fonts.md,
-                marginTop: 5,
+                marginTop: 15,
                 fontWeight: 'bold',
             }}>{item.employer_name}</Text>
 
